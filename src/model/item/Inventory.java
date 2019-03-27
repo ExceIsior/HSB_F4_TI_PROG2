8b9cc84aa8ae75b46db75788e7da7b03b56527bf
ExSynth <isiworld@hotmail.de>
2019-03-27 18:53:33 +0100

    Inventory.java updated
    Inventory Class implemented.
    Item Class updated.

--- src/model/item/Inventory.java
+++ src/model/item/Inventory.java
@@ -1,5 +1,20 @@
 package model.item;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 public class Inventory {
+    HashMap<String, Integer> inventory = new HashMap<>();
+
+    public Inventory(HashMap<String, Integer> inventory) {
+        this.inventory = inventory;
+    }
+
+    public void addItem(Item item){
+        inventory.put(item.getName(), this.getAmountOfItem(item)+1);
+    }
     
+    public int getAmountOfItem(Item item){
+        return this.inventory.get(item.getName());
+    }
 }
--- src/model/item/Item.java
+++ src/model/item/Item.java
@@ -1,5 +1,19 @@
 package model.item;
 
 public class Item {
+    private int rarity = 0;
+    private String name = "";
+
+    public Item(int rarity, String name) {
+        
+    }
+
+    public int getRarity() {
+        return rarity;
+    }
+
+    public String getName() {
+        return name;
+    }
     
 }
