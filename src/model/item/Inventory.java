9a8634a7685e23c9c5d4da51df7bef110a7de17c
ExceIsior <48092243+ExceIsior@users.noreply.github.com>
2019-03-27 19:41:04 +0100

    Inventory Klasse ausformuliert

--- src/model/item/Inventory.java
+++ src/model/item/Inventory.java
@@ -1,5 +1,17 @@
 package model.item;
 
+import java.util.HashMap;
+
 public class Inventory {
     
+    static HashMap<Item, Integer> inventory = new HashMap<>();
+    public static void main (String[] args)
+    {
+        addItem(new Item());
+    }
+    public static void addItem(Item i)
+    {
+        inventory.put(i, inventory.containsKey(i) ? inventory.get(i)+1 : 1);
+        System.out.println(inventory.get(i));
+    }
 }
