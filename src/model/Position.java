725938b95071658f79a2eedadea84c7320d493a5
ExSynth <isiworld@hotmail.de>
2019-03-27 17:20:36 +0100

    Position; Character;
    Defined Position Attributes
    Implemented Position Methods
    Defined Character Attributes

--- src/model/Position.java
+++ src/model/Position.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Position {
-    
+    private int x = 0;
+    private int y = 0;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
--- src/model/gameObject/Character.java
+++ src/model/gameObject/Character.java
@@ -1,5 +1,13 @@
 package model.gameObject;
 
+import java.util.ArrayList;
+
 public class Character extends Interactive {
+    private String name = "";
+    private int HealthPoints = 0;
+    private int agility = 0;
+    private Status = null;
+    private ArrayList<Skill> = null;
+    
     
 }
