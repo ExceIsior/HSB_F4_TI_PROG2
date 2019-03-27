18c5d25d98e343d5bee30e4590643159b9fcfe3a
ExSynth <isiworld@hotmail.de>
2019-03-27 19:40:21 +0100

    Updated Item.Java
    Item implemented

--- src/model/item/Item.java
+++ src/model/item/Item.java
@@ -1,11 +1,14 @@
 package model.item;
 
+import java.util.Objects;
+
 public class Item {
     private int rarity = 0;
     private String name = "";
 
     public Item(int rarity, String name) {
-        
+        this.name = name;
+        this.rarity = rarity;
     }
 
     public int getRarity() {
@@ -15,5 +18,32 @@ public class Item {
     public String getName() {
         return name;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
