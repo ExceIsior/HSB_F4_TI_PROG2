a141579e01adbf399e246590b522f07a237ff5e8
diegoo14 <48996916+diegoo14@users.noreply.github.com>
2019-03-27 17:15:50 +0100

    Interactive ausformuliert

--- src/model/gameObject/Interactive.java
+++ src/model/gameObject/Interactive.java
@@ -1,7 +1,11 @@
 package model.gameObject;
 
 import model.gameObject.GameObject;
+import model.item.Item;
 
 public class Interactive extends GameObject {
     
+    private int damage = 0;
+    private int armor = 0;
+    private Item loot = null;
 }
