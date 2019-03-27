43ffa5faa92c2362eff353a31ee8dbc967304a76
Niklas <niklas.b2013@gmail.com>
2019-03-27 18:10:37 +0100

    Hero + Skill + Effekt Klassen hinzufügen

--- src/model/gameObject/Hero.java
+++ src/model/gameObject/Hero.java
@@ -1,5 +1,10 @@
 package model.gameObject;
 
+import model.Effekt;
+
 public class Hero extends Character {
-    
+    private int surgeValue = 0;
+    private Effekt status = null;
+    private Skill skill = null;
+    private Item equipment = null;
 }
