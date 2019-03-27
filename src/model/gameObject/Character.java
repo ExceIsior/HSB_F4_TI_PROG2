e9718f7973037f2553d393caf38243385a006e71
ExSynth <isiworld@hotmail.de>
2019-03-27 17:31:19 +0100

    Character; Position
    Character and Position edited.

--- src/model/gameObject/Character.java
+++ src/model/gameObject/Character.java
@@ -1,6 +1,5 @@
 package model.gameObject;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class Character extends Interactive {
@@ -9,10 +8,5 @@ public class Character extends Interactive {
     private int agility = 0;
     private Status = null;
     private ArrayList<Skill> = null;
-    
-=======
-public abstract class Character extends Interactive {
->>>>>>> 7f1d2832cb48c2f6e14a0d025566759ef404bfb1
-    
 
 }
