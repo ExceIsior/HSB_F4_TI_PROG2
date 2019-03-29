d618d4e789622125c180f1f869a0b73a6f9be16b
ExSynth <isiworld@hotmail.de>
2019-03-29 15:00:28 +0100

    Constants Updated
    Added directory and resources paths

--- src/control/Constants.java
+++ src/control/Constants.java
@@ -7,6 +7,18 @@ public interface Constants {
     public static final int TILE_SIZE_X = 4;
     public static final int TILE_SIZE_Y = 4;
     
+    public static final String MAPS_PATH = "./maps/";
+    public static final String RESSOURCE_PATH = "./res/";
+    public static final String CHARACTERS_PATH = RESSOURCE_PATH + "characters/";
+    public static final String OBJECTS_PATH = RESSOURCE_PATH + "objects/";
+    public static final String ITEMS_PATH = RESSOURCE_PATH + "items/";
     
+    public static final String MOUNTAIN_PATH = OBJECTS_PATH + "M";
+    public static final String TREE_PATH = OBJECTS_PATH + "T";
+    public static final String TRAP_PATH = OBJECTS_PATH + "t";
+    public static final String TREASURE_PATH = OBJECTS_PATH + "C";
     
+    public static final String HERO_PATH =  CHARACTERS_PATH + "H";
+    public static final String BOSS_PATH =  CHARACTERS_PATH + "B";
+    public static final String VILLAIN_PATH =  CHARACTERS_PATH + "V";
 }
