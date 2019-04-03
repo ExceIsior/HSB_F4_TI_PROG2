13aab332e958635c6f174cadd1cc0d3a25a8e4ee
ExSynth <isiworld@hotmail.de>
2019-04-03 16:02:45 +0200

    Objective.java updated

--- src/model/Objective.java
+++ src/model/Objective.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 import model.gameObject.GameObject;
 
 public class Objective  {
-    private HashMap<GameObject, Integer> objective;
+    private HashMap<GameObject, Integer> objectives;
+
+    public Objective(HashMap<GameObject, Integer> objectives) {
+        this.objectives = objectives;
+    }
+    
     public boolean objectiveComplete(){
         return true;
     }
