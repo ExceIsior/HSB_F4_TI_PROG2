6512eeb11871076c633edb760bf9b7ada1818ecb
ExceIsior <48092243+ExceIsior@users.noreply.github.com>
2019-04-03 15:24:26 +0200

    fixed imports

--- src/model/Objective.java
+++ src/model/Objective.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.HashMap;
+import model.gameObject.GameObject;
+
 public class Objective  {
     private HashMap<GameObject, Integer> objective;
     public boolean objectiveComplete(){
