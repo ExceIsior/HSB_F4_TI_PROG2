5c2815eb5c8c0bb86168f5534df3bf81394d170c
Niklas <niklas.b2013@gmail.com>
2019-04-03 15:10:58 +0200

    Update DiceController.java

--- src/control/DiceController.java
+++ src/control/DiceController.java
@@ -2,13 +2,15 @@
 
 package control;
 
+import control.Constants.Const;
+
 import java.util.Random;
 
 public class DiceController {
 
     public static int castDie(){
         Random random = new Random();
-        return (int) (random.nextFloat() * 20);
+        return (int) (random.nextFloat() * Const.NUMBER_CUBE_FACES);
     }
 
     public static void main(String[] args){
