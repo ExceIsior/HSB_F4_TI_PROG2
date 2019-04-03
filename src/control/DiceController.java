a7a688f8e1540b8398402f550229250416cfffcc
Niklas <niklas.b2013@gmail.com>
2019-04-03 09:41:58 +0200

    castDie Methode hinzufügen

--- src/control/DiceController.java
+++ src/control/DiceController.java
@@ -2,6 +2,16 @@
 
 package control;
 
+import java.util.Random;
+
 public class DiceController {
-    
+
+    public static int castDie(){
+        Random random = new Random();
+        return (int) (random.nextFloat() * 20);
+    }
+
+    public static void main(String[] args){
+        System.out.println(DiceController.castDie());
+    }
 }
