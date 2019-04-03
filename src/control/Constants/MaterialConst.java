09114308ad3e37918d94e6665ba1877b40049d92
ExSynth <isiworld@hotmail.de>
2019-04-03 17:30:52 +0200

    Materials enumeration created; Skills updated

---
+++ src/control/Constants/MaterialConst.java
@@ -0,0 +1,27 @@
+package control.Constants;
+
+public interface MaterialConst {
+
+    public static final int RARITY_COMMON = 0;
+    public static final int RARITY_RARE = 1;
+    public static final int RARITY_EXQUISITE = 2;
+    public static final int RARITY_LEGENDARY = 3;
+
+    public static final String BRONZE_NAME = "Bronze";
+    public static final int BRONZE_RARITY = RARITY_COMMON;
+
+    public static final String IRON_NAME = "Iron";
+    public static final int IRON_RARITY = RARITY_RARE;
+
+    public static final String PLATIN_NAME = "Platin";
+    public static final int PLATIN_RARITY = RARITY_EXQUISITE;
+
+    public static final String CLOTH_NAME = "Cloth";
+    public static final int CLOTH_RARITY = RARITY_COMMON;
+
+    public static final String LEATHER_NAME = "Leather";
+    public static final int LEATHER_RARITY = RARITY_RARE;
+
+    public static final String AMETHYST_NAME = "Amethyst";
+    public static final int AMETHYST_RARITY = RARITY_RARE;
+}
--- src/model/gameObject/Skill.java
+++ src/model/gameObject/Skill.java
@@ -1,17 +1,22 @@
 package model.gameObject;
 
-class Skill
-{
+import java.util.HashMap;
+
+class Skill {
+
     private String name = "";
     private int damage = 0;
     private int dieBonus = 0;
     private int range = 0;
-    
-    public Skill(String name, int damage, int dieBonus, int range){
+    private int radius = 0;
+    private int availability = 0;
+
+    public Skill(String name, int damage, int dieBonus, int range, int damageRadius, int availability) {
         this.name = name;
         this.damage = damage;
         this.dieBonus = dieBonus;
         this.range = range;
+        this.availability = availability;
     }
 
     public String getName() {
@@ -25,10 +30,25 @@ class Skill
     public int getDieBonus() {
         return dieBonus;
     }
-    
+
     public int getRange() {
         return range;
     }
-    
-    
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
+
 }
