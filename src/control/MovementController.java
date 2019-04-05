387490a3813b8881c209ef37d72ef0f14e70e637
ExceIsior <48092243+ExceIsior@users.noreply.github.com>
2019-04-05 18:56:30 +0200

    Pthfinder Alg.

--- src/control/MovementController.java
+++ src/control/MovementController.java
@@ -1,9 +1,11 @@
 package control;
 
+import com.sun.tools.javac.util.ArrayUtils;
 import control.Constants.Const;
 import model.Position;
 import model.gameObject.GameObject;
 import model.gameObject.Hero;
+import model.gameObject.Character;
 import model.map.Dungeon;
 import model.map.Tile;
 
@@ -65,5 +67,39 @@ public class MovementController
         int xCoordinate = (position.getX() / Const.TILE_SIZE_X);
         int yCoordinate = (position.getY() / Const.TILE_SIZE_Y);
         return dungeon.getTile(new Position(xCoordinate, yCoordinate));
+    }
+    
+    
+    //TODO
+    public Position[] pathfinder(Position currentPosition, Position endPosition, Character c)
+    {
+        Position[] range = null;
+        Position[] rangex = null;
+        Position[] rangey = null;
+        Position[] ranged = null;
+        //Position ipose = new Position(currentPosition.getX(),currentPosition.getY());
+        
+        for (int i=0; i < c.getAgility(); i++)
+        {
+            rangex[i] = new Position(currentPosition.getX()+i, currentPosition.getY()  );
+            rangey[i] = new Position(currentPosition.getX()  , currentPosition.getY()+i);
+            ranged[i] = new Position(currentPosition.getX()+i, currentPosition.getY()+i);
+            //path[i] = new Position(currentPosition.getX()+i, currentPosition.getY()+i);
+        }
+        for (int i=0; i < c.getAgility(); i++)
+        {
+            range =  combine(rangex, rangey, ranged);
+        }
+        return range;
+    }
+    
+    public static Position[] combine(Position[] a, Position[] b, Position[] c)
+    {
+        int length = a.length + b.length + c.length;
+        Position[] result = new Position[length];
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        System.arraycopy(c, 0, result, a.length + b.length, c.length);
+        return result;
     }
 }
