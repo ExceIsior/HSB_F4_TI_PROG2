f9b3970a50cb8369d83ed76aadac9e42a6dc5229
ExSynth <isiworld@hotmail.de>
2019-04-05 10:16:48 +0200

    Materials ENumeration Added

---
+++ src/control/ActionController.java
@@ -0,0 +1,10 @@
+package control;
+
+import model.map.Field;
+import model.gameObject.Skill;
+
+public class ActionController {
+    public void attack(Skill skill, Field targetField){
+        
+    }
+}
