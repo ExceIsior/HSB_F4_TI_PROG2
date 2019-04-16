018e9807eed3507f407ff0952ec012a381fd05ad
diegoo14 <48996916+diegoo14@users.noreply.github.com>
2019-04-16 20:27:49 +0200

    Grundmaps geschrieben

--- src/control/Main.java
+++ src/control/Main.java
@@ -9,14 +9,14 @@ import utilities.JsonParser;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        JsonParser.toJsonFile(MapController.generateDefaultMap(), "./maps/map1/map.json");
-//        Tile[][] map = (Tile[][]) JsonParser.fromJsonFile(Tile[][].class, "./maps/map1/map.json");
-//        for (int i = 0; i < 5; i++) {
-//            for (int j = 0; j < 5; j++) {
-//                map[i][j].setVisible(true);
-//            }
-//        }
-//        System.out.println(new Dungeon(null, map));
+        JsonParser.toJsonFile(MapController.generateDefaultMap(), "./maps/map3/map.json");
+        Tile[][] map = (Tile[][]) JsonParser.fromJsonFile(Tile[][].class, "./maps/map3/map.json");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                map[i][j].setVisible(true);
+            }
+        }
+        System.out.println(new Dungeon(null, map));
 
          GameController game = new GameController();
          game.start();
